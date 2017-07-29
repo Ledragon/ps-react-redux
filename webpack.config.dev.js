23f862945ff0b3b03535c687150e1cf1afdb7df5
Hugues Stefanski <hugues.stefanski@gmail.com>
2017-07-29 18:10:33 +0200

    Live serving

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -33,9 +33,10 @@ module.exports = {
             { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/octet-stream' },
             { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=image/svg+xml' }
         ]
-    },
-    externals: {
-        "react": "React",
-        "react-dom": "ReactDOM"
     }
+    // ,
+    // externals: {
+    //     "react": "React",
+    //     "react-dom": "ReactDOM"
+    // }
 }
