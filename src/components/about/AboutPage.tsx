08908b4b60271fd03cd76f0eb8e9e8b68ab7380f
Hugues Stefanski <hugues.stefanski@gmail.com>
2017-07-30 18:20:42 +0200

    About page

---
+++ src/components/about/AboutPage.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+
+class AboutPage extends React.Component {
+    render(): false | JSX.Element {
+        return (
+            <div>
+                <h1>About</h1>
+                <p>This application uses React, Readux, React Router, and a variety of other helpful libraries.</p>
+            </div>
+        );
+    }
+}
