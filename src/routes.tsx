24a400522891a34226cfe21120dc328168974e13
Hugues Stefanski <hugues.stefanski@gmail.com>
2017-07-30 18:33:47 +0200

    Routes file

---
+++ src/routes.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import { Route } from 'react-router';
+import App from './components/App';
+import HomePage from './components/home/HomePage';
+import AboutPage from './components/about/AboutPage';
+
+export default (
+    <Route path="/" component={App}>
+        <Route exact component={HomePage}></Route>
+        <Route path="about" component={AboutPage}></Route>
+    </Route>
+)
