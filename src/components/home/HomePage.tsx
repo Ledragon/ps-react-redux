3a08517603bcf051348902ccf4398aa470edd29a
Hugues Stefanski <hugues.stefanski@gmail.com>
2017-07-30 18:15:16 +0200

    Home page missing packages

---
+++ src/components/home/HomePage.tsx
@@ -0,0 +1,20 @@
+import * as React from 'react';
+import { Link } from 'react-router-dom';
+
+export interface IHomePageProps {
+
+}
+
+class HomePage extends React.Component<IHomePageProps>{
+    render(): false | JSX.Element {
+        return (
+            <div className='jumbotron'>
+                <h1>Pluralsight Administration</h1>
+                <p>React, Redux, and React Router in TypeScript for ultra-responsive web apps.</p>
+                <Link to='about' className='btn btn-primary btn-lg'></Link>
+            </div>
+        );
+    }
+}
+
+export default HomePage;
