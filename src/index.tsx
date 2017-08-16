d7a402d5ac0826f68cbd873b861700a69c4fee75
Hugues Stefanski <hugues.stefanski@gmail.com>
2017-08-16 12:36:05 +0200

    Initialize and use store

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,21 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
 
 import App from './components/App';
+import configureStore from './store/configureStore';
 
 import '../styles.css';
 require('../node_modules/bootstrap/dist/css/bootstrap.css');
 
+const store = configureStore();
+
 ReactDOM.render(
-    <Router>
-        <App />
-    </Router>,
+    <Provider store={store}>
+        <Router>
+            <App />
+        </Router>
+    </Provider>    ,
     document.getElementById('example')
 )
