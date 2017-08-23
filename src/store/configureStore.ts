bc10fc6e1c17cf1eaffa3c02680c6332dbd4404a
Hugues Stefanski <hugues.stefanski@gmail.com>
2017-08-23 08:57:29 +0200

    Fix module problem

--- src/store/configureStore.ts
+++ src/store/configureStore.ts
@@ -1,5 +1,7 @@
+var result;
 if (process.env.NODE_ENV === 'production') {
-    module.exports = require('./configureStore.prod')
+    result = require('./configureStore.prod')
 } else {
-    module.exports = require('./configureStore.dev')
+    result = require('./configureStore.dev')
 }
+export default result;
